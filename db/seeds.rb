b4767b6327dbe129659af5678a51fe7b4ff9c042
Ryan Allen <rcallen89@gmail.com>
2020-02-03 15:45:42 -0700

    Add Pets to Seed for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Pet.destroy_all
+Shelter.destroy_all
+
 
 shelter1 = Shelter.create!(name: "Mike's Shelter", address: "1331 17th Street", city: "Denver", state: "CO", zip: "80202")
 shelter2 = Shelter.create!(name: "Meg's Shelter", address: "150 Main Street", city: "Hershey", state: "PA", zip: "17003")
 
-# pet1 = shelter1.pets.create!(name: "Tron",
-#                        image: "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/13002248/GettyImages-187066830.jpg",
-#                        approximate_age: 3,
-#                        sex: "Male",
-#                        description: 'Too Cool for School')
-# pet2 = shelter2.pets.create!(name: "Kat",
-#                   image: "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/13002248/GettyImages-187066830.jpg",
-#                   approximate_age: 2,
-#                   sex: "Female",
-#                   description: 'Too Hot for TV')
+pet1 = shelter1.pets.create!(name: "Tron",
+                       image: "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/13002248/GettyImages-187066830.jpg",
+                       approximate_age: 3,
+                       sex: "Male",
+                       description: 'Too Cool for School')
+pet2 = shelter2.pets.create!(name: "Kat",
+                  image: "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/13002248/GettyImages-187066830.jpg",
+                  approximate_age: 2,
+                  sex: "Female",
+                  description: 'Too Hot for TV')
