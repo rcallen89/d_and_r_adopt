b20e8640f55ec18596fd74d525c690eb6a0b5b94
DeMarcus Kirby <tkd024@gmail.com>
2020-02-05 10:43:38 -0700

    add helper method im application controller for amount of favorites

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :fav_count
+
+  def fav_count
+    if session[:favorites]
+      session[:favorites].keys.count 
+    else
+      0
+    end
+  end
 end
