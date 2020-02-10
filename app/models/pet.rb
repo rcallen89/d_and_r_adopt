f64c1063f871349bd916ac906af4e0fc1dd554c4
Ryan Allen <rcallen89@gmail.com>
2020-02-10 15:13:19 -0700

    Add Dependent Destory for Joins

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,7 +2,7 @@ class Pet < ApplicationRecord
   validates_presence_of :name, :description, :approximate_age, :sex
 
   belongs_to :shelter
-  has_many :pet_adopt_forms
+  has_many :pet_adopt_forms, dependent: :destroy 
   has_many :adopt_forms, through: :pet_adopt_forms
 
   enum adoptable: %w(Available Pending Adopted)
