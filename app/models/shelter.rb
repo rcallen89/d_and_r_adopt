dc815b902215d2bb67fb62d4a9fab7dab3475835
DeMarcus Kirby <tkd024@gmail.com>
2020-02-11 12:45:33 -0700

    Add num_pets, avg_score, and num_apps method for shelter model

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -3,4 +3,16 @@ class Shelter < ApplicationRecord
 
   has_many :pets
   has_many :reviews
+
+  def num_pets
+    pets.count
+  end
+
+  def avg_score
+   reviews.average(:rating)
+  end
+
+  def num_apps
+    pets.joins(:pet_adopt_forms).count
+  end
 end
